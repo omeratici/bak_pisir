df62ac3982a471bef0437ea12fc8b6420f3e251f
Serceo <serhancan1903@gmail.com>
2022-05-29 18:14:31 +0300

    giris login kaydol first

--- lib/main.dart
+++ lib/main.dart
@@ -10,15 +10,12 @@ import 'Users.dart';
 import 'UsersCevap.dart';
 import 'dolabim.dart';
 
-
-
 void main() {
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
-
 
   @override
   Widget build(BuildContext context) {
@@ -28,14 +25,12 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home:  GirisEkrani(),
+      home: GirisEkrani(),
     );
   }
 }
 
 class GirisEkrani extends StatefulWidget {
-
-
   @override
   State<GirisEkrani> createState() => _GirisEkraniState();
 }
@@ -47,122 +42,182 @@ class _GirisEkraniState extends State<GirisEkrani> {
   var aktifKullanici;
   String _errorMessage = '';
 
-  Users parseUsersCevap(String cevap){
+  Users parseUsersCevap(String cevap) {
     var jsonVeri = json.decode(cevap);
-    var user =Users(0,"xx","0","0","xx");
-    if(jsonVeri["success"] as int==1){
+    var user = Users(0, "xx", "0", "0", "xx");
+    if (jsonVeri["success"] as int == 1) {
       var userCevap = UsersCevap.fromJson(jsonVeri);
-      List<Users> userList =userCevap.userList;
+      List<Users> userList = userCevap.userList;
       user = userList[0];
     }
     return user;
   }
 
-  Future<Users> userGet (String a,String b) async {
-    var url =Uri.parse("http://213.14.130.80/bakpisir/Usersget3.php");
-    var veri = {"userName":a, "password":b};
-    var cevap = await http.post(url,body: veri);
+  Future<Users> userGet(String a, String b) async {
+    var url = Uri.parse("http://213.14.130.80/bakpisir/Usersget3.php");
+    var veri = {"userName": a, "password": b};
+    var cevap = await http.post(url, body: veri);
 
     return parseUsersCevap(cevap.body);
   }
-  Future<void> login(String a,String b) async {
-    var aktifKullanici = await userGet(a,b);
-    if(aktifKullanici.userId != 0){
+
+  Future<void> login(String a, String b) async {
+    var aktifKullanici = await userGet(a, b);
+    if (aktifKullanici.userId != 0) {
       print(aktifKullanici.userId);
       print(aktifKullanici.userName);
-      Navigator.push(context, MaterialPageRoute(builder: (context) => anaSAyfa()));
-    }else {
-     ScaffoldMessenger.of(context).showSnackBar(
-         SnackBar(content: Text("Kullanıcı Adi veya Şifre Hatalı")),
-     );
+      Navigator.push(
+          context, MaterialPageRoute(builder: (context) => anaSAyfa()));
+    } else {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text("Kullanıcı Adi veya Şifre Hatalı")),
+      );
     }
-
   }
-@override
+
+  @override
   void initState() {
-
     super.initState();
-
-
   }
+
   @override
   Widget build(BuildContext context) {
-
-    return Scaffold(
-      key:scaffoldKey,
-      appBar: AppBar(
-        title: Text("Bak Pişir - Giriş"),
-      ),
-      body: Center(
-        child: Padding(
-          padding: const EdgeInsets.all(20.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              TextFormField(
-                keyboardType: TextInputType.emailAddress,
-                decoration: InputDecoration(labelText: 'Email'),
-                controller: tfKullaniciAdi,
-                onChanged: (val){
-                  validateEmail(val);
-                },
-              ),
-              TextFormField(
-                keyboardType: TextInputType.visiblePassword,
-                decoration: InputDecoration(labelText: 'Şifre'),
-                obscureText: true,
-                controller: tfKSifre,
-              ),
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Text(_errorMessage, style: TextStyle(color: Colors.red),),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(15.0),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceAround,
-                  children:[
-                    ElevatedButton(onPressed: (){
-                      login(tfKullaniciAdi.text,tfKSifre.text);
-
-
-
-                    }, child: Text("Giriş Yap")),
-                    ElevatedButton(onPressed: (){
-
-                      Navigator.push(context, MaterialPageRoute(builder: (context) => uyeOl()));
-                    }, child: Text("Üye Ol")),
-                  ]
-                ),
-              ),
-
-              TextButton(onPressed: (){
-                Navigator.push(context, MaterialPageRoute(builder: (context) => sifremiUnuttum()));
-              }, child: Text("Şifremi Unuttum"))
-
-            ],
+    return Container(
+      decoration: BoxDecoration(
+          image: DecorationImage(
+              image: AssetImage('assets/giris.png'), fit: BoxFit.cover)),
+      child: Scaffold(
+        backgroundColor: Colors.transparent,
+        body: Stack(children: [
+          Container(
+            padding: EdgeInsets.only(left: 35, top: 130),
+            child: Text(
+              "Hoşgeldiniz",
+              style: TextStyle(color: Colors.white, fontSize: 33),
+            ),
           ),
-        ),
+          SingleChildScrollView(
+            child: Container(
+              padding: EdgeInsets.only(
+                  top: MediaQuery.of(context).size.height * 0.5,
+                  right: 35,
+                  left: 35),
+              child: Column(
+                children: [
+                  TextFormField(
+                    keyboardType: TextInputType.emailAddress,
+                    decoration: InputDecoration(
+                        hintText: 'Email',
+                        border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(10)),
+                        fillColor: Colors.grey.shade100,
+                        filled: true),
+                    controller: tfKullaniciAdi,
+                    onChanged: (val) {
+                      validateEmail(val);
+                    },
+                  ),
+                  SizedBox(
+                    height: 30,
+                  ),
+                  TextFormField(
+                    keyboardType: TextInputType.visiblePassword,
+                    decoration: InputDecoration(
+                        hintText: 'Şifre',
+                        border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(10)),
+                        fillColor: Colors.grey.shade100,
+                        filled: true),
+                    obscureText: true,
+                    controller: tfKSifre,
+                  ),
+                  SizedBox(
+                    height: 40,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        "Giriş Yap",
+                        style: TextStyle(
+                          color: Color.fromRGBO(175, 68, 72, 1),
+                          fontSize: 27,
+                          fontWeight: FontWeight.w700,
+                        ),
+                      ),
+                      CircleAvatar(
+                        radius: 30,
+                        backgroundColor: Color.fromRGBO(175, 68, 72, 1),
+                        child: IconButton(
+                          color: Colors.white,
+                          onPressed: () {
+                            login(tfKullaniciAdi.text, tfKSifre.text);
+                          },
+                          icon: Icon(Icons.arrow_forward),
+                        ),
+                      )
+                    ],
+                  ),
+                  SizedBox(
+                    height: 40,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      TextButton(
+                          onPressed: () {
+                            Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (context) => uyeOl()));
+                          },
+                          child: Text(
+                            "Kayıt Ol",
+                            style: TextStyle(
+                              decoration: TextDecoration.underline,
+                              fontSize: 18,
+                              color: Color.fromRGBO(175, 68, 72, 1),
+                            ),
+                          )),
+                      TextButton(
+                          onPressed: () {
+                            Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (context) => sifremiUnuttum()));
+                          },
+                          child: Text(
+                            "Şifremi Unuttum",
+                            style: TextStyle(
+                              decoration: TextDecoration.underline,
+                              fontSize: 18,
+                              color: Color.fromRGBO(175, 68, 72, 1),
+                            ),
+                          ))
+                    ],
+                  )
+                ],
+              ),
+            ),
+          ),
+        ]),
       ),
-
     );
   }
+
   void validateEmail(String val) {
-    if(val.isEmpty){
+    if (val.isEmpty) {
       setState(() {
         _errorMessage = "Email adresini boş geçemezsiniz !";
       });
-    }else if(!EmailValidator.validate(val, true)){
+    } else if (!EmailValidator.validate(val, true)) {
       setState(() {
         _errorMessage = "Geçersiz Email Adresi";
       });
-    }else{
+    } else {
       setState(() {
-
         _errorMessage = "";
       });
     }
   }
-
-
 }
--- lib/uyeOl.dart
+++ lib/uyeOl.dart
@@ -7,7 +7,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:http/http.dart' as http;
 import 'package:flutter_pw_validator/flutter_pw_validator.dart';
-
+import 'UsersCevap.dart';
 import 'Users.dart';
 
 class uyeOl extends StatefulWidget {
@@ -29,25 +29,188 @@ class _uyeOlState extends State<uyeOl> {
   var scaffoldKey = GlobalKey<ScaffoldState>();
   String _errorMessage = '';
 
-  Future<void> insertUser (String userName,String password, String email ) async {
-
+  Future<void> insertUser(
+      String userName, String password, String email) async {
     var url = Uri.parse("http://213.14.130.80/bakpisir/insert_user.php");
-    var veri = {"userName": userName, "password": password, "email": email,"userRole":"0"};
+    var veri = {
+      "userName": userName,
+      "password": password,
+      "email": email,
+      "userRole": "0"
+    };
     var cevap = await http.post(url, body: veri);
     var jsonVeri = json.decode(cevap.body);
-    if(jsonVeri["success"] as int==1){
-      Navigator.push(context, MaterialPageRoute(builder: (context) => mailOnay()));
-    }else{
+    if (jsonVeri["success"] as int == 1) {
+      Navigator.push(
+          context, MaterialPageRoute(builder: (context) => mailOnay()));
+    } else {
       ScaffoldMessenger.of(context).showSnackBar(
         SnackBar(content: Text("Kayıt Başarısız Tekrar Deneyin")),
       );
     }
     print(cevap.body.toString());
-
   }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    {
+      return Container(
+        decoration: BoxDecoration(
+            image: DecorationImage(
+                image: AssetImage('assets/kayit.png'), fit: BoxFit.cover)),
+        child: Scaffold(
+          backgroundColor: Colors.transparent,
+          body: Stack(children: [
+            Container(
+              padding: EdgeInsets.only(left: 35, top: 100),
+              child: Text(
+                "Hesap\nOluştur",
+                style: TextStyle(color: Colors.white, fontSize: 33),
+              ),
+            ),
+            SingleChildScrollView(
+              child: Container(
+                padding: EdgeInsets.only(
+                    top: MediaQuery.of(context).size.height * 0.3,
+                    right: 35,
+                    left: 35),
+                child: Column(
+                  children: [
+                    TextFormField(
+                      decoration: InputDecoration(
+                        hintText: 'Kullanıcı Adı',
+                        hintStyle: TextStyle(color: Colors.white),
+                        enabledBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(10),
+                            borderSide: BorderSide(color: Colors.white)),
+                        focusedBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(10),
+                            borderSide: BorderSide(color: Colors.black)),
+                      ),
+                      controller: tfKullaniciAdi,
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: FlutterPwValidator(
+                        controller: tfKullaniciAdi,
+                        minLength: 6,
+                        width: 400,
+                        height: 40,
+                        onSuccess: () {
+                          print("oldu");
+                          _isUserNameValidate = true;
+                        },
+                        onFail: () {
+                          print("olmadı");
+                          _isUserNameValidate = false;
+                        },
+                      ),
+                    ),
+                    TextFormField(
+                      controller: tfemail,
+                      keyboardType: TextInputType.emailAddress,
+                      decoration: InputDecoration(
+                        hintText: 'Email',
+                        hintStyle: TextStyle(color: Colors.white),
+                        enabledBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(10),
+                            borderSide: BorderSide(color: Colors.white)),
+                        focusedBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(10),
+                            borderSide: BorderSide(color: Colors.black)),
+                      ),
+                      onChanged: (val) {
+                        validateEmail(val);
+                      },
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Text(
+                        _errorMessage,
+                        style: _isMailValidate
+                            ? TextStyle(color: Colors.green)
+                            : TextStyle(color: Colors.red),
+                      ),
+                    ),
+                    TextFormField(
+                      obscureText: !_isVisible,
+                      decoration: InputDecoration(
+                        suffixIcon: IconButton(
+                            onPressed: () {
+                              setState(() {
+                                _isVisible = !_isVisible;
+                              });
+                            },
+                            icon: _isVisible
+                                ? Icon(Icons.visibility, color: Colors.white)
+                                : Icon(Icons.visibility_off,
+                                    color: Colors.grey)),
+                        hintText: 'Şifre',
+                        hintStyle: TextStyle(color: Colors.white),
+                        enabledBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(10),
+                            borderSide: BorderSide(color: Colors.white)),
+                        focusedBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(10),
+                            borderSide: BorderSide(color: Colors.black)),
+                      ),
+                      controller: tfKSifre,
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(3.0),
+                      child: FlutterPwValidator(
+                        controller: tfKSifre,
+                        minLength: 6,
+                        uppercaseCharCount: 2,
+                        numericCharCount: 3,
+                        specialCharCount: 1,
+                        width: 400,
+                        height: 120,
+                        onSuccess: () {
+                          print("oldu");
+                          _isPasswordValidate = true;
+                        },
+                        onFail: () {
+                          print("olmadı");
+                          _isPasswordValidate = false;
+                        },
+                      ),
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text(
+                          "Kayıt Ol",
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 27,
+                            fontWeight: FontWeight.w700,
+                          ),
+                        ),
+                        CircleAvatar(
+                          radius: 30,
+                          backgroundColor: Color.fromRGBO(175, 68, 72, 1),
+                          child: IconButton(
+                            color: Colors.white,
+                            onPressed: () {
+                              insertUser(tfKullaniciAdi.text, tfKSifre.text,
+                                  tfemail.text);
+                            },
+                            icon: Icon(Icons.arrow_forward),
+                          ),
+                        )
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ]),
+        ),
+      );
+    }
+
+    /*  return Scaffold(
       key:scaffoldKey,
       appBar: AppBar(
         title: Text("Bak Pişir - Üye Ol"),
@@ -182,28 +345,30 @@ class _uyeOlState extends State<uyeOl> {
     );
   }
 
+  
+*/
+  }
+
   void validateEmail(String val) {
-    if(val.isEmpty){
+    if (val.isEmpty) {
       _isMailValidate = false;
       setState(() {
         _errorMessage = "Email adresini boş geçemezsiniz !";
       });
-    }else if(!EmailValidator.validate(val, true)){
+    } else if (!EmailValidator.validate(val, true)) {
       setState(() {
         _errorMessage = "Geçersiz Email Adresi";
       });
-    }else if(EmailValidator.validate(val, true)){
+    } else if (EmailValidator.validate(val, true)) {
       _isMailValidate = true;
       setState(() {
         _errorMessage = "Geçerli Mail Adresi";
       });
-    }else{
+    } else {
       _isMailValidate = false;
       setState(() {
         _errorMessage = "";
       });
     }
   }
-
 }
-
