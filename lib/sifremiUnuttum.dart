7142d677b23bfad274cb00a37e739d0e575fbbb1
Serceo <serhancan1903@gmail.com>
2022-05-29 19:24:44 +0300

    sifremi unuttum page

--- lib/sifremiUnuttum.dart
+++ lib/sifremiUnuttum.dart
@@ -10,8 +10,61 @@ class sifremiUnuttum extends StatefulWidget {
 class _sifremiUnuttumState extends State<sifremiUnuttum> {
   var tfemail = TextEditingController();
   var scaffoldKey = GlobalKey<ScaffoldState>();
+
   @override
   Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+          image: DecorationImage(
+              image: AssetImage('assets/kayit.png'), fit: BoxFit.cover)),
+      child: Scaffold(
+        backgroundColor: Colors.transparent,
+        body: Stack(children: [
+          Container(
+            padding: EdgeInsets.only(left: 35, top: 130),
+            child: Text(
+              "Şifremi Unuttum",
+              style: TextStyle(color: Colors.white, fontSize: 33),
+            ),
+          ),
+          SingleChildScrollView(
+            child: Container(
+              padding: EdgeInsets.only(
+                  top: MediaQuery.of(context).size.height * 0.5,
+                  right: 35,
+                  left: 35),
+              child: Column(
+                children: [
+                  TextFormField(
+                    controller: tfemail,
+                    keyboardType: TextInputType.emailAddress,
+                    decoration: InputDecoration(
+                        hintText: 'Email',
+                        border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(10)),
+                        fillColor: Colors.grey.shade100,
+                        filled: true),
+                  ),
+                  SizedBox(height: 40),
+                  ElevatedButton(
+                    onPressed: () {},
+                    child: const Text('Yeni Şifre Gönder'),
+                    style: ElevatedButton.styleFrom(
+                        primary: (Color.fromRGBO(229, 57, 53, 1)),
+                        fixedSize: const Size(200, 50),
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(50))),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ]),
+      ),
+    );
+  }
+
+  /*{
     return Scaffold(
       key:scaffoldKey,
       appBar: AppBar(
@@ -45,4 +98,5 @@ class _sifremiUnuttumState extends State<sifremiUnuttum> {
 
     );
   }
+}*/
 }
