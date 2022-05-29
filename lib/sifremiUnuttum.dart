2db47d3f1814b50d663e82582e103dd2e31bd002
Serceo <serhancan1903@gmail.com>
2022-05-29 19:25:22 +0300

    sifremi unuttum vol2

--- lib/sifremiUnuttum.dart
+++ lib/sifremiUnuttum.dart
@@ -63,40 +63,4 @@ class _sifremiUnuttumState extends State<sifremiUnuttum> {
       ),
     );
   }
-
-  /*{
-    return Scaffold(
-      key:scaffoldKey,
-      appBar: AppBar(
-        title: Text("Bak Pişir - Şifremi Unuttum"),
-      ),
-      body: Center(
-        child: Padding(
-          padding: const EdgeInsets.all(20.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-
-              TextField(
-                controller: tfemail,
-                decoration: InputDecoration(
-                  hintText: "e-mail Adresinizi Giriniz",
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(20.0),
-                child: ElevatedButton(onPressed: (){
-
-
-                }, child: Text("Yeni Şifre Gönder")),
-              ),
-
-            ],
-          ),
-        ),
-      ),
-
-    );
-  }
-}*/
 }
