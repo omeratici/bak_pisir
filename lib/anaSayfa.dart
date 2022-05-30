a76dabf5898ff785694a641b12a7ac8b93ef4451
Serceo <serhancan1903@gmail.com>
2022-05-30 14:10:07 +0300

    TabBar AppBar

--- lib/anaSayfa.dart
+++ lib/anaSayfa.dart
@@ -2,6 +2,7 @@ import 'dart:convert';
 
 import 'package:bak_pisir/Foodtypes.dart';
 import 'package:bak_pisir/Widgets/MyDrawer.dart';
+import 'package:bak_pisir/assets/db_icons.dart';
 import 'package:bak_pisir/urunListe.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
@@ -20,18 +21,16 @@ class anaSAyfa extends StatefulWidget {
 
 class _anaSAyfaState extends State<anaSAyfa> {
   var scaffoldKey = GlobalKey<ScaffoldState>();
-  List<Foodtypes>typesList=[];
+  List<Foodtypes> typesList = [];
 
-  List<Foodtypes> parsetypesCevap(String cevap){
+  List<Foodtypes> parsetypesCevap(String cevap) {
     var jsonVeri = json.decode(cevap);
 
-    if(jsonVeri["success"] as int==1){
+    if (jsonVeri["success"] as int == 1) {
       var typesCevap = FoodtypesCevap.fromJson(jsonVeri);
-      typesList =typesCevap.typesList;
-      setState(() {
-
-      });
-    }else{
+      typesList = typesCevap.typesList;
+      setState(() {});
+    } else {
       ScaffoldMessenger.of(context).showSnackBar(
         SnackBar(content: Text("Yemek Türleri Yüklenemedi Tekrar Deneyin")),
       );
@@ -39,51 +38,73 @@ class _anaSAyfaState extends State<anaSAyfa> {
     return typesList;
   }
 
-  Future<List<Foodtypes>> typesGet () async {
+  Future<List<Foodtypes>> typesGet() async {
     print("typeGet çalıştı");
-    var url =Uri.parse("http://213.14.130.80/bakpisir/FoodtypesGet.php");
+    var url = Uri.parse("http://213.14.130.80/bakpisir/FoodtypesGet.php");
     var cevap = await http.get(url);
     print(cevap.body);
     return parsetypesCevap(cevap.body);
   }
 
-@override
+  @override
   void initState() {
     super.initState();
     typesGet();
   }
 
-
-
   @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Bak Pişir - Ana Sayfa"),
-      ),
-      drawer: MyDrawer(widget.aktifKullnaici),
-      key:scaffoldKey,
-
-      body:  GridView.builder(
-          gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
-              maxCrossAxisExtent: 200,
-              childAspectRatio: 3 / 2,
-              crossAxisSpacing: 20,
-              mainAxisSpacing: 20),
-          itemCount: typesList.length,
-          itemBuilder: (context,index){
-            return Container(
+  Widget build(BuildContext context) => DefaultTabController(
+        length: 4,
+        child: Scaffold(
+          appBar: AppBar(
+            title: Text("Ana Sayfa"),
+            centerTitle: true,
+            actions: [
+              IconButton(
+                icon: Icon(Icons.notifications_none),
+                onPressed: () {},
+              ),
+              IconButton(
+                icon: Icon(Icons.search),
+                onPressed: () {},
+              )
+            ],
+            backgroundColor: Colors.red.shade300,
+            flexibleSpace: Container(
+              decoration: BoxDecoration(
+                  gradient: LinearGradient(
+                colors: [Colors.purple.shade500, Colors.red.shade500],
+                begin: Alignment.bottomRight,
+                end: Alignment.topLeft,
+              )),
+            ),
+            bottom:
+                TabBar(indicatorColor: Colors.white, indicatorWeight: 3, tabs: [
+              Tab(icon: Icon(Icons.home), text: 'Anasayfa'),
+              Tab(icon: Icon(DBIcons.fridge), text: 'Dolabım'),
+              Tab(icon: Icon(Icons.book_sharp), text: 'Tariflerim'),
+              Tab(icon: Icon(Icons.settings), text: 'Ayarlar'),
+            ]),
+          ),
+          drawer: MyDrawer(widget.aktifKullnaici),
+          key: scaffoldKey,
+          body: GridView.builder(
+            gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+                maxCrossAxisExtent: 200,
+                childAspectRatio: 3 / 2,
+                crossAxisSpacing: 20,
+                mainAxisSpacing: 20),
+            itemCount: typesList.length,
+            itemBuilder: (context, index) {
+              return Container(
                 alignment: Alignment.center,
-
                 child: Text(typesList[index].typeName),
-            decoration: BoxDecoration(
-            color: Colors.amber,
-            borderRadius: BorderRadius.circular(15)),
-            );
-          },
-
-      ),
-
-    );
-  }
+                decoration: BoxDecoration(
+                    color: Colors.amber,
+                    borderRadius: BorderRadius.circular(15)),
+              );
+            },
+          ),
+        ),
+      );
 }
---
+++ lib/assets/db_icons.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/widgets.dart';
+import 'package:flutter/material.dart';
+
+class DBIcons {
+  DBIcons._();
+
+  static const _kFontFam = 'DBIcons';
+  static const String? _kFontPkg = null;
+
+  static const IconData recipes =
+      IconData(0xe801, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData fridge =
+      IconData(0xe800, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+}
